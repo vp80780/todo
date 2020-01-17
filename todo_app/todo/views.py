e19262ab7c57796055b49a98c823c827ae2eeb2a
vijendra <vijendra@tersesoft.com>
2020-01-17 21:49:13 +0530

    changes

--- todo_app/todo/views.py
+++ todo_app/todo/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-
+from rest_framework.response import Response
 from todo_app.todo.models import Task
 from todo_app.todo.serializers import TaskSerializer
 
@@ -7,3 +7,7 @@ from todo_app.todo.serializers import TaskSerializer
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+class RequiredBankDocuments(APIView):
+    def get(self, request):
+        return Response({'result': 'fail', 'message': 'Something went wrong'})
--- todo_app/urls.py
+++ todo_app/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import url, include
 
 from rest_framework.routers import DefaultRouter
 
-from todo_app.todo.views import TaskViewSet
+from todo_app.todo.views import TaskViewSet, RequiredBankDocuments
 
 router = DefaultRouter()
 router.register(r"todo", TaskViewSet)
 
 urlpatterns = [
     url(r"^api/", include((router.urls, "todo"))),
+    url(r'^api/required-bank-documents/', RequiredBankDocuments.as_view()),
 ]
